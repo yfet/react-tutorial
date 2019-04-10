074d6cbea83d147ddb654aff271b8048390ca376
Tefy Andriambelonjatovo <tefy.nahary@gmail.com>
2019-04-10 12:47:55 +0300

    Start Login Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    login: "",
+    password: ""
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.login, this.state.password);
+  }
+
+  loginChange = (event) => {
+    console.log(event.target.value);
+    this.setState({login: event.target.value});
+  }
+
+  passwordChange = (event) => {
+    console.log(event.target.value);
+    this.setState({password: event.target.value});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form>
+          <div>
+            <label>Login</label>
+            <input type="text" value={this.state.login} onChange={this.loginChange} />
+          </div>
+          <div>
+            <label>Password</label>
+            <input type="password" value={this.state.password} onChange={this.passwordChange} />
+          </div>
+          <button>Reset</button>
+          <button onClick={this.onSubmit}>Submit</button>
+        </form>
       </div>
     );
   }
