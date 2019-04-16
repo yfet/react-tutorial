61c710643eeea99f58baef4e59f95b760ce41ba4
tefy andriambelonjatovo <tefy.nahary@gmail.com>
2019-04-16 22:55:01 +0300

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,7 @@ import "firebase/auth";
 
 import UserContext from "./components/UserContext";
 
-const config = {
-  apiKey: "AIzaSyDr9BdBruWpkl2X6-bFg2Yx7WE1B0KeQBY",
-  authDomain: "react-tutorial-184a1.firebaseapp.com",
-  databaseURL: "https://react-tutorial-184a1.firebaseio.com",
-  projectId: "react-tutorial-184a1",
-  storageBucket: "react-tutorial-184a1.appspot.com",
-  messagingSenderId: "970091748057"
-};
+const config = {};
 
 firebase.initializeApp(config);
 
