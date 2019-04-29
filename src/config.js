5ab639e87034de2503280bf2c489f9a1a5c07442
tefy andriambelonjatovo <tefy.nahary@gmail.com>
2019-04-29 20:03:54 +0300

    Update firebase config

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyDr9BdBruWpkl2X6-bFg2Yx7WE1B0KeQBY",
+    authDomain: "react-tutorial-184a1.firebaseapp.com",
+    databaseURL: "https://react-tutorial-184a1.firebaseio.com",
+    projectId: "react-tutorial-184a1",
+    storageBucket: "react-tutorial-184a1.appspot.com",
+    messagingSenderId: "970091748057"
+};
+  
+export  { config };
